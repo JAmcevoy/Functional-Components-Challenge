f9cb3fe1bacc87a52e031bbf3b0180b74d84c35f
Joshua McEvoy <144149195+JAmcevoy@users.noreply.github.com>
2024-06-05 11:38:22 +0000

    Using CSS Modules with React

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,14 @@
+import css from "./css/Sidebar.module.css"
+import React from 'react'
+
+function Sidebar() {
+    return (
+        <div className = {css.sidebar}>
+            <a href = "#." target= "_blank">My Photos</a>
+            <a href = "#." target= "_blank">My Illustrations</a>
+            <a href = "#." target= "_blank">My Paintings</a>
+        </div>
+    )
+}
+
+export default Sidebar
