2b82abac1b4d7e3e4eaf7270551e00c3847cdd8c
Joshua McEvoy <144149195+JAmcevoy@users.noreply.github.com>
2024-06-06 09:52:00 +0000

    What is 'State'? finsihed

--- my-app/src/components/StatefulGreeting.js
+++ my-app/src/components/StatefulGreeting.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React from "react";  // Importing the React library
 
-class StatefulGreeting extends React.Component{
+// Defining a stateful class component
+class StatefulGreeting extends React.Component {
+    // Constructor method to initialize state and props
+    constructor(props) {
+        super(props);  // Calling the parent class's constructor with props
+        // Initializing the state
+        this.state = {
+            introduction: "Hello!",  // Initial state value for introduction
+            buttonText: "Exit",  // Initial state value for buttonText
+        };
+    }
+
+    // Render method to display the component
     render() {
-        return <h1>Hello {this.props.name}, {this.props.greeting}!</h1>
+        return (
+            <div>
+                {/* Displaying the state and props values in the h1 element */}
+                <h1>{this.state.introduction} {this.props.name}, {this.props.greeting}</h1>
+                {/* Displaying the state value in the button */}
+                <button>{this.state.buttonText}</button>
+            </div>
+        );
     }
 }
 
+// Exporting the StatefulGreeting component as the default export
 export default StatefulGreeting;
