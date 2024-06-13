57ccb5503a6adbf92e47a143475147c16628b821
Joshua McEvoy <144149195+JAmcevoy@users.noreply.github.com>
2024-06-13 10:38:24 +0000

    Component Lifecycles Challenge - with comments_

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -2,42 +2,47 @@ import React, { Component } from "react"; // Importing React library and Compone
 import css from "./css/Content.module.css"; // Importing CSS Module for styling
 import { savedPosts } from "../posts.json"; // Importing savedPosts array from posts.json file
 import PostItem from "./PostItem"; // Importing the child component PostItem
+import Loader from "./Loader"; // Importing the Loader component
 
 // Defining a class component called Content
 export class Content extends Component {
     constructor(props) {
         super(props); // Calling the constructor of the parent class (Component)
+
+        // Initializing the state
+        // Why: To manage whether the posts are loaded or not
+        this.state = {
+            isLoaded: false
+        };
     }
 
+    // Lifecycle method called after the component is mounted
+    // Why: To simulate an asynchronous data fetch
+    // How: Sets isLoaded to true after a delay of 2 seconds
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                isLoaded: true,
+            });
+        }, 2000);
+    }
+
+    // Render method to display the component
+    // Why: To render the UI based on the current state
+    // How: Displays a loader until the data is loaded, then displays the posts
     render() {
         return (
-            <div className={css.Content}> {/* Rendering a div with className from CSS Module */}
-                <div className={css.TitleBar}> {/* Rendering a div for the title bar */}
+            <div className={css.Content}> {/* Applying the 'Content' CSS class from the CSS module */}
+                <div className={css.TitleBar}> {/* Applying the 'TitleBar' CSS class from the CSS module */}
                     <h1>My Photos</h1> {/* Rendering a heading */}
                 </div>
 
-                <div className={css.SearchResults}> {/* Rendering a div for search results */}
-                    {/* Part 1: Creating the map function to render posts */}
-                    {/* Mapping through the savedPosts array to render each post */}
-                    {/* For each post, rendering a div with post details */}
-                    {/* Key attribute is used for optimization and performance */}
-                    {/* This part is commented out because it's replaced by Part 2 */}
-                    {/* {
-                        savedPosts.map((post) => {
-                            return (
-                                <div className={css.SearchItem} key={post.title}>
-                                    <p>{post.title}</p>
-                                    <p>{post.name}</p>
-                                    <img src={post.image} alt="random" />
-                                    <p>{post.description}</p>
-                                </div>
-                            );
-                        })
-                    } */}
-
-                    {/* Part 2: Rendering the child component PostItem */}
-                    {/* Passing savedPosts array as props to the child component */}
-                    <PostItem savedPosts={savedPosts} />
+                <div className={css.SearchResults}> {/* Applying the 'SearchResults' CSS class from the CSS module */}
+                    {
+                        this.state.isLoaded ? // Conditional rendering based on the isLoaded state
+                        <PostItem savedPosts={savedPosts} /> // Rendering PostItem component with savedPosts as a prop
+                        : <Loader /> // Rendering Loader component if the data is not yet loaded
+                    }
                 </div>
             </div>
         );
