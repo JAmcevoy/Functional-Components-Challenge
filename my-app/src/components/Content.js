1b31e222498ecd07ab99b83473f8acd4ab4523b6
Joshua McEvoy <144149195+JAmcevoy@users.noreply.github.com>
2024-06-13 16:08:39 +0000

    SearchBar Challenge - done

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -1,52 +1,66 @@
-import React, { Component } from "react"; // Importing React library and Component from 'react' module
-import css from "./css/Content.module.css"; // Importing CSS Module for styling
-import { savedPosts } from "../posts.json"; // Importing savedPosts array from posts.json file
-import PostItem from "./PostItem"; // Importing the child component PostItem
-import Loader from "./Loader"; // Importing the Loader component
+import React, { Component } from "react";
+import css from "./css/Content.module.css";
+import {savedPosts} from "../posts.json";
+import PostItem from "./PostItem";
+import Loader from "./Loader";
 
-// Defining a class component called Content
 export class Content extends Component {
     constructor(props) {
-        super(props); // Calling the constructor of the parent class (Component)
-
-        // Initializing the state
-        // Why: To manage whether the posts are loaded or not
+        super(props)
         this.state = {
-            isLoaded: false
-        };
+            isLoaded: false,
+            posts: [],
+        }
     }
 
-    // Lifecycle method called after the component is mounted
-    // Why: To simulate an asynchronous data fetch
-    // How: Sets isLoaded to true after a delay of 2 seconds
     componentDidMount() {
-        setTimeout(() => {
+        setTimeout(()=>{
             this.setState({
                 isLoaded: true,
-            });
-        }, 2000);
+                posts: savedPosts,
+            })
+        }, 2000)
     }
 
-    // Render method to display the component
-    // Why: To render the UI based on the current state
-    // How: Displays a loader until the data is loaded, then displays the posts
+    handleChange = (e) => {
+        const name = e.target.value.toLowerCase();
+        const filteredPosts = savedPosts.filter((post)=>{
+            return post.name.toLowerCase().includes(name);
+        })
+        
+        this.setState({
+            posts: filteredPosts
+        })
+    }
+    
     render() {
         return (
-            <div className={css.Content}> {/* Applying the 'Content' CSS class from the CSS module */}
-                <div className={css.TitleBar}> {/* Applying the 'TitleBar' CSS class from the CSS module */}
-                    <h1>My Photos</h1> {/* Rendering a heading */}
+            <div className={css.Content}>
+                
+                <div className={css.TitleBar}>
+                    <h1>My Photos</h1>
+                    <form>
+                        <label htmlFor='searchinput'>Search</label>
+                        <input 
+                        type='search' 
+                        id='searchinput' 
+                        placeholder='By Author'
+                        onChange={(e) => this.handleChange(e)}
+                        />
+                        <h4>posts found {this.state.posts.length}</h4>
+                    </form>
                 </div>
 
-                <div className={css.SearchResults}> {/* Applying the 'SearchResults' CSS class from the CSS module */}
+                <div className={css.SearchResults}>
                     {
-                        this.state.isLoaded ? // Conditional rendering based on the isLoaded state
-                        <PostItem savedPosts={savedPosts} /> // Rendering PostItem component with savedPosts as a prop
-                        : <Loader /> // Rendering Loader component if the data is not yet loaded
+                        this.state.isLoaded ?
+                        <PostItem savedPosts={this.state.posts} />
+                        : <Loader />
                     }
                 </div>
             </div>
-        );
+        )
     }
 }
 
-export default Content; // Exporting the Content component as the default export
+export default Content
