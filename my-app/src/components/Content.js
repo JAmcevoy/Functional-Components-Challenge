06a2e5c2f8ad5808962e386b0f18c74ea760a901
Joshua McEvoy <144149195+JAmcevoy@users.noreply.github.com>
2024-06-13 16:08:57 +0000

    SearchBar Challenge - done

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -1,66 +1,69 @@
-import React, { Component } from "react";
-import css from "./css/Content.module.css";
-import {savedPosts} from "../posts.json";
-import PostItem from "./PostItem";
-import Loader from "./Loader";
+import React, { Component } from "react"; // Importing React library and Component from 'react' module
+import css from "./css/Content.module.css"; // Importing CSS Module for styling
+import { savedPosts } from "../posts.json"; // Importing savedPosts array from posts.json file
+import PostItem from "./PostItem"; // Importing the child component PostItem
+import Loader from "./Loader"; // Importing the Loader component
 
+// Defining a class component called Content
 export class Content extends Component {
     constructor(props) {
-        super(props)
+        super(props);
         this.state = {
-            isLoaded: false,
-            posts: [],
-        }
+            isLoaded: false, // State to manage if data is loaded
+            posts: [], // State to hold the list of posts
+        };
     }
 
+    // Simulating data fetching after component mounts
     componentDidMount() {
-        setTimeout(()=>{
+        setTimeout(() => {
             this.setState({
                 isLoaded: true,
-                posts: savedPosts,
-            })
-        }, 2000)
+                posts: savedPosts, // Setting state with data from savedPosts
+            });
+        }, 2000); // Simulating a 2-second delay
     }
 
+    // Handler for input change in the search field
     handleChange = (e) => {
-        const name = e.target.value.toLowerCase();
-        const filteredPosts = savedPosts.filter((post)=>{
-            return post.name.toLowerCase().includes(name);
-        })
-        
+        const name = e.target.value.toLowerCase(); // Get input value and convert to lowercase
+        const filteredPosts = savedPosts.filter((post) => {
+            return post.name.toLowerCase().includes(name); // Filter posts by name based on input
+        });
+
         this.setState({
-            posts: filteredPosts
-        })
-    }
-    
+            posts: filteredPosts, // Update state with filtered posts
+        });
+    };
+
+    // Render method to display the component
     render() {
         return (
-            <div className={css.Content}>
-                
-                <div className={css.TitleBar}>
-                    <h1>My Photos</h1>
+            <div className={css.Content}> {/* Main container for the content */}
+                <div className={css.TitleBar}> {/* Container for the title bar */}
+                    <h1>My Photos</h1> {/* Title */}
                     <form>
-                        <label htmlFor='searchinput'>Search</label>
-                        <input 
-                        type='search' 
-                        id='searchinput' 
-                        placeholder='By Author'
-                        onChange={(e) => this.handleChange(e)}
+                        <label htmlFor='searchinput'>Search</label> {/* Label for the search input */}
+                        <input
+                            type='search'
+                            id='searchinput'
+                            placeholder='By Author' // Placeholder text
+                            onChange={(e) => this.handleChange(e)} // Event handler for input change
                         />
-                        <h4>posts found {this.state.posts.length}</h4>
+                        <h4>Posts found: {this.state.posts.length}</h4> {/* Display number of posts found */}
                     </form>
                 </div>
 
-                <div className={css.SearchResults}>
+                <div className={css.SearchResults}> {/* Container for the search results */}
                     {
-                        this.state.isLoaded ?
-                        <PostItem savedPosts={this.state.posts} />
-                        : <Loader />
+                        this.state.isLoaded ? // Check if data is loaded
+                        <PostItem savedPosts={this.state.posts} /> // Display posts if loaded
+                        : <Loader /> // Display loader if data is not loaded
                     }
                 </div>
             </div>
-        )
+        );
     }
 }
 
-export default Content
+export default Content; // Exporting the Content component as the default export
